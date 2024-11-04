4a143404a602e9254a134d86df4c4111674fc0a6
Hope <boukhatemabdelilah420@gmail.com>
2024-11-04 14:12:26 +0100

    add dotenv file

--- P2PLearningAPI/Program.cs
+++ P2PLearningAPI/Program.cs
@@ -1,6 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using P2PLearningAPI.Models;
 
+using DotNetEnv;
+
+// b- Load environment variables from .env file
+DotNetEnv.Env.Load();
+
+// Retrieve the connection string components from the environment variables
+var serverName = Environment.GetEnvironmentVariable("SQL_SERVER_NAME");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var connectionString = $"Server={serverName};Database={dbName};Trusted_Connection=True;TrustServerCertificate=True;";
+Console.WriteLine($"Server Name: {serverName}, Database Name: {dbName}, Connection String: {connectionString}");
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +20,7 @@ builder.Services.AddControllers();
 
 // Define and register the DbContext directly with the connection string
 builder.Services.AddDbContext<P2PLearningDbContext>(options =>
-   options.UseSqlServer("Server=DESKTOP-RR3AB05\\SQLEXPRESS;Database=P2PLearningDB;Trusted_Connection=True;TrustServerCertificate=True;"));
+   options.UseSqlServer(connectionString));
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
